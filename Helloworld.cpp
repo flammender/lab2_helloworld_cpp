ff35f8a498c8eb8cb8fea9ab53772d111b69f1bc
flammender <softy.snowflakes@hotmail.com>
2025-05-01 21:03:39 +0300

    Added comments to Helloworld program

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << "!" << std::endl;
-    return 0;
+    std::string name;   //переменная для хранения имени пользователя
+    std::cout << "Enter your name: ";   //запрос имени пользователя
+    std::cin >> name;   //чтения ввода пользователя
+    std::cout << "Hello world from " << name << "!" << std::endl;   //вывод приветствия
+    return 0;   //завершение программы
 {
